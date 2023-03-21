accdc46679c22d18fee111ebd0a232267e1353e5
emPeeGee <eempeegee@gmail.com>
2023-03-21 13:00:51 +0200

    feat(fe): Add eslint and prettier setup

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './frontend/tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      // Tells eslint-plugin-react to automatically detect the version of React to use.
+      version: 'detect'
+    },
+    // Tells eslint how to resolve imports
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off'
+  }
+};
