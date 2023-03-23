70741ea210f0010703f9a9bdb6d532733a10b33e
emPeeGee <eempeegee@gmail.com>
2023-03-23 16:19:59 +0200

    feat(fe): Not found page redesign

--- frontend/src/components/layout/NotFound/NotFound.tsx
+++ frontend/src/components/layout/NotFound/NotFound.tsx
@@ -1,28 +1,79 @@
 import React from 'react';
-import { Button, Container, Paper, Text, Title } from '@mantine/core';
-import { IconArrowBarLeft } from '@tabler/icons-react';
+import {
+  createStyles,
+  Image,
+  Container,
+  Title,
+  Text,
+  Button,
+  SimpleGrid,
+  rem
+} from '@mantine/core';
+import image from 'assets/404.svg';
 import { Link } from 'react-router-dom';
 
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: rem(120),
+    paddingBottom: rem(120)
+  },
+
+  title: {
+    fontWeight: 900,
+    fontSize: rem(34),
+    marginBottom: theme.spacing.md,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: rem(32)
+    }
+  },
+
+  control: {
+    [theme.fn.smallerThan('sm')]: {
+      width: '100%'
+    }
+  },
+
+  mobileImage: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none'
+    }
+  },
+
+  desktopImage: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none'
+    }
+  }
+}));
+
 export function NotFound() {
+  const { classes } = useStyles();
+
   return (
-    <Container size="md">
-      <Paper shadow="xs" p="md" my="md" withBorder>
-        <Title order={1} my="md">
-          Page Not Found
-        </Title>
-        <Text my="md">
-          Looks like you&apos;ve followed a broken link or entered a URL that doesn&apos;t exist on
-          this app.
-        </Text>
-        <Button<typeof Link>
-          to="/"
-          component={Link}
-          variant="outline"
-          my="md"
-          leftIcon={<IconArrowBarLeft />}>
-          Back to main page
-        </Button>
-      </Paper>
+    <Container className={classes.root}>
+      <SimpleGrid spacing={80} cols={2} breakpoints={[{ maxWidth: 'sm', cols: 1, spacing: 40 }]}>
+        <Image src={image} className={classes.mobileImage} />
+        <div>
+          <Title className={classes.title}>Something is not right...</Title>
+          <Text color="dimmed" size="lg">
+            Page you are trying to open does not exist. You may have mistyped the address, or the
+            page has been moved to another URL. If you think this is an error contact support.
+          </Text>
+
+          <Button
+            to="/"
+            component={Link}
+            variant="outline"
+            size="md"
+            mt="xl"
+            className={classes.control}>
+            Get back to home page
+          </Button>
+        </div>
+        <Image src={image} className={classes.desktopImage} />
+      </SimpleGrid>
     </Container>
   );
 }
