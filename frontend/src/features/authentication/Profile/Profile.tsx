890c3d211694e1603514ee231c1c70dfaf8dd3ed
emPeeGee <eempeegee@gmail.com>
2023-03-24 21:53:33 +0200

    feat(fe): Make Profile look better

--- frontend/src/features/authentication/Profile/Profile.tsx
+++ frontend/src/features/authentication/Profile/Profile.tsx
@@ -8,14 +8,21 @@ import {
   Flex,
   Table,
   createStyles,
-  Avatar
+  Avatar,
+  rem,
+  Paper
 } from '@mantine/core';
-import { IconAt, IconPhoneCall } from '@tabler/icons-react';
+import { IconAt, IconCalendarBolt, IconEdit, IconPhoneCall } from '@tabler/icons-react';
 import { UserContext } from 'features/authentication/user.context';
 import avatar from 'assets/logo.png';
 import { UserModel } from '../authentication.model';
 
 const useStyles = createStyles((theme) => ({
+  tableHeader: {
+    '& th': {
+      fontWeight: 'bold'
+    }
+  },
   icon: {
     color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[5]
   },
@@ -25,11 +32,16 @@ const useStyles = createStyles((theme) => ({
   }
 }));
 
+// TODO: Profile edit
+
 export function Profile() {
   const userContext = useContext(UserContext);
   const { username, name, email, phone, createdAt, latestLogins, updatedAt } =
     userContext?.user as UserModel;
   const { classes } = useStyles();
+
+  const createdDate = new Date(createdAt).toTimeString();
+  const updatedDate = new Date(updatedAt).toTimeString();
 
   const latestLoginsRows = latestLogins.map((row, idx) => {
     const date = new Date(row).toTimeString();
@@ -44,86 +56,69 @@ export function Profile() {
 
   return (
     <Container my={32}>
-      <Title order={1}>Profile</Title>
+      <Title order={1} py={rem('2rem')}>
+        Profile
+      </Title>
       <Flex direction="column">
-        <Group noWrap>
-          <Avatar src={avatar} size={94} radius="md" />
-          <div>
-            <Text fz="xs" tt="uppercase" fw={700} c="dimmed">
-              {username}
-            </Text>
-
-            <Text fz="lg" fw={500} className={classes.name}>
-              {name}
-            </Text>
-
-            <Group noWrap spacing={10} mt={3}>
-              <IconAt stroke={1.5} size="1rem" className={classes.icon} />
-              <Text fz="xs" c="dimmed">
-                {email}
+        <Paper withBorder p={rem('2rem')} mb={rem('2rem')} radius="md">
+          <Group noWrap>
+            <Avatar src={avatar} size={94} radius="md" />
+            <div>
+              <Text fz={rem('1.5rem')} fw={500} className={classes.name}>
+                {name}
               </Text>
-            </Group>
 
-            <Group noWrap spacing={10} mt={5}>
-              <IconPhoneCall stroke={1.5} size="1rem" className={classes.icon} />
-              <Text fz="xs" c="dimmed">
-                {phone}
+              <Text fz={rem('1.25rem')} tt="uppercase" fw={700} c="dimmed">
+                {username}
               </Text>
-            </Group>
-          </div>
-        </Group>
 
-        <Group>
-          <Text>Your name: </Text>
-          <Code color="violet" my="xs" style={{ fontSize: '20px' }}>
-            {userContext?.user?.username}
-          </Code>
-        </Group>
+              <Group noWrap spacing={10} mt={3}>
+                <IconAt stroke={1.5} size="1.5rem" className={classes.icon} />
+                <Text fz={rem('1.25rem')} c="dimmed">
+                  {email}
+                </Text>
+              </Group>
 
-        <Group>
-          <Text>Your username: </Text>
-          <Code color="primary" my="xs" style={{ fontSize: '20px' }}>
-            {userContext?.user?.name}
-          </Code>
-        </Group>
+              <Group noWrap spacing={10} mt={5}>
+                <IconPhoneCall stroke={1.5} size="1.5rem" className={classes.icon} />
+                <Text fz={rem('1.25rem')} c="dimmed">
+                  {phone}
+                </Text>
+              </Group>
 
-        <Group>
-          <Text>User created at: </Text>
-          <Code color="yellow" my="xs" style={{ fontSize: '20px' }}>
-            {createdAt}
-          </Code>
-        </Group>
+              <Group noWrap spacing={10} mt={5}>
+                <IconCalendarBolt stroke={1.5} size="1.5rem" className={classes.icon} />
+                <Text fz={rem('1.25rem')}>Created at: </Text>
+                <Text fz={rem('1.25rem')} c="dimmed">
+                  {createdDate}
+                </Text>
+              </Group>
 
-        <Group>
-          <Text>Last user update: </Text>
-          <Code color="yellow" my="xs" style={{ fontSize: '20px' }}>
-            {updatedAt}
-          </Code>
-        </Group>
+              <Group noWrap spacing={10} mt={5}>
+                <IconEdit stroke={1.5} size="1.5rem" className={classes.icon} />
+                <Text fz={rem('1.25rem')}>Last update: </Text>
+                <Text fz={rem('1.25rem')} c="dimmed">
+                  {updatedDate}
+                </Text>
+              </Group>
+            </div>
+          </Group>
+        </Paper>
 
-        <Group>
-          <Text>Your email: </Text>
-          <Code color="primary" my="xs" style={{ fontSize: '20px' }}>
-            {userContext?.user?.email}
-          </Code>
-        </Group>
-
-        <Group>
-          <Text>Your phone: </Text>
-          <Code color="primary" my="xs" style={{ fontSize: '20px' }}>
-            {phone}
-          </Code>
-        </Group>
-
-        <Table miw={700}>
-          <thead>
-            <tr>
-              <th>Number</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>{latestLogins}</tbody>
-        </Table>
+        <Flex direction="column" mb={rem('2rem')}>
+          <Title order={2} py={rem('2rem')}>
+            Latest logins
+          </Title>
+          <Table miw={700}>
+            <thead className={classes.tableHeader}>
+              <tr>
+                <th>#</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody>{latestLoginsRows}</tbody>
+          </Table>
+        </Flex>
       </Flex>
     </Container>
   );
