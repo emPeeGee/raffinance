ed71190100b584e3be755a1caff9eacbe98a1c02
emPeeGee <eempeegee@gmail.com>
2023-04-01 11:30:46 +0300

    feat(be):  🏆  Enable delete in cors

--- backend/internal/cors/middleware.go
+++ backend/internal/cors/middleware.go
@@ -10,7 +10,7 @@ import (
 func Handler() gin.HandlerFunc {
 	return cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "OPTIONS"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
