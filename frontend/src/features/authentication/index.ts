df7ad9b9d7a63a830dcd3a38776fb9c3e803c633
emPeeGee <eempeegee@gmail.com>
2023-04-22 10:45:47 +0300

    chore(fe):  🦛 Delete deprecated file

--- frontend/src/features/authentication/index.ts
+++ frontend/src/features/authentication/index.ts
@@ -5,7 +5,6 @@ import {
 } from 'features/authentication/authentication.model';
 import { Profile } from 'features/authentication/Profile/Profile';
 import { SignIn } from 'features/authentication/SignIn/SignIn';
-import { UserContext, UserContextModel, UserProvider } from 'features/authentication/user.context';
 
-export { Profile, SignIn, UserContext, UserProvider };
-export type { UserModel, CredentialsModel, AuthenticationResponse, UserContextModel };
+export { Profile, SignIn };
+export type { UserModel, CredentialsModel, AuthenticationResponse };
